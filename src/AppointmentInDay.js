d8f3b620e64f5490c71399b1bd6e446ec6f40919
Jasper Scharff <jas-scharff@live.nl>
2022-11-30 15:20:55 +0100

    Update

--- src/AppointmentInDay.js
+++ src/AppointmentInDay.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+
 const format_time = time => (time < 10 ? `0${time}:00u` : `${time}:00u`);
 
 export default ({ time, patient, dentist, assistant }) => (
   <li className="appointment">
     <div className="time">{format_time(time)}</div>
     <div className="patient">Patiënt: {patient}</div>
-    <div className="dentist">Tandarts: {dentist}</div>
+    <div className="dentist">
+      Tandarts: {dentist} <br>
+      </br> `Email: ${ ${denistName.surname}@${randomEmailProvider.mail}}  <br>
+      </br> Telefoon: {dentist.telefoon}
+      </div>
     <div className="assistant">Assistent: {assistant}</div>
   </li>
 );
