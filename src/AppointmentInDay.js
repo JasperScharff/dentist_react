c3565cba27ac54dd3134d8453f795f4b8c2a933b
Jasper Scharff <jas-scharff@live.nl>
2022-12-02 15:18:36 +0100

    Update

--- src/AppointmentInDay.js
+++ src/AppointmentInDay.js
@@ -1,17 +1,32 @@
 import React from "react";
 
+const format_time = (time) => (time < 10 ? `0${time}:00u` : `${time}:00u`);
 
-const format_time = time => (time < 10 ? `0${time}:00u` : `${time}:00u`);
-
-export default ({ time, patient, dentist, assistant, email }) => (
-  <li className="appointment">
-    <div className="time">{format_time(time)}</div>
-    <div className="patient">Patiënt: {patient}</div>
-    <div className="dentist">
-      Tandarts: {dentist} <br>
-      </br>Email: ${dentist.email}<br>
-      </br> Telefoon: {dentist.telefoon}
+export default function AppointmentInDay({
+  time,
+  patient,
+  dentist,
+  assistant,
+}) {
+  return (
+    <li className="appointment">
+      <div className="time">{format_time(time)}</div>
+      <div className="patient">
+      <b>Patient:</b> {patient.name} <br></br>
+        <b>Email: </b> {patient.email}
+        <br></br> <b>Telefoon: </b> {`06-${patient.telNum}`}
       </div>
-    <div className="assistant">Assistent: {assistant}</div>
-  </li>
-);
+      <div className="dentist">
+        <b>Tandarts:</b> {dentist.name} <br></br>
+        <b>Email: </b> {dentist.email}
+        <br></br> <b>Telefoon: </b> {`06-${dentist.telNum}`}
+      </div>
+      <div>
+      <b>Assistant:</b> {assistant.name} <br></br>
+      <b>Email: </b> {assistant.email}
+      <br></br> <b>Telefoon: </b> {`06-${assistant.telNum}`}
+      </div>
+    </li>
+
+  );
+}
